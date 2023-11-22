f0329386b84d2fd02f5a7cb7a56f58a75035a7e7
Jhons Albornoz Araya <jhons.albornoz.araya@gmail.com>
2023-11-22 19:59:51 -0300

    First commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import { AddCategory, GifGrid } from './components';
+
+export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState([ 'Tokyo Revengers' ]);
+
+  const onAddCategory = ( newCategory ) => {
+    if (categories.includes(newCategory)) return;
+    setCategories([newCategory, ...categories]);
+    
+  }
+
+  return (
+    <>
+        <h1>Buscador de gifs</h1>
+
+        <AddCategory
+          onNewCategory={ value => onAddCategory(value) }
+        />
+
+        {
+          categories.map( category => (
+            <GifGrid
+              key={ category }
+              category={ category }
+            />
+          ))
+        }
+    </>
+  )
+}
